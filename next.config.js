38b2c13e70a63eb3634a55da686e1043e1ab9898
nick19890714 <83580980+nick19890714@users.noreply.github.com>
2022-01-18 17:50:25 +0800

    update

--- next.config.js
+++ next.config.js
@@ -2,16 +2,16 @@ module.exports = {
   reactStrictMode: true,
   basePath: '/nextjs_github',
   assetPrefix: '/nextjs_github',
-  images: {
-    // domains: ['nextjs_github'],
-    loader: "imgix",
-    path: "",
-  },
+  // images: {
+  //   // domains: ['nextjs_github'],
+  //   loader: "imgix",
+  //   path: "",
+  // },
   exportPathMap: async function (defaultPathMap, { dev, dir, outDir, distDir, buildId }) {
     return {
       '/': { page: '/' },
       '/new': { page: '/new' },
-      // '/edit': { page: '/edit' },
+      '/api/hello': { page: '/api/hello' },
       // '/preview': { page: '/preview' }
     }
   }

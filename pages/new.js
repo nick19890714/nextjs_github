ae2da987ed896b1d939bcb57e7f603c1a851ff15
nick19890714 <83580980+nick19890714@users.noreply.github.com>
2022-01-19 14:23:21 +0800

    Update new.js

--- pages/new.js
+++ pages/new.js
@@ -3,14 +3,18 @@ import React, { useEffect, useState } from "react";
 
 const New = () => {
     const [data, setData] = useState();
-    useEffect(() => {
+    useEffect(async () => {
         if (!data) {
-            const res = fetch('');
-            const datas = res.json();
-            setData(123);
+            const res = await fetch('http://localhost:3000/api/hello');
+            if (res.status === 200) {
+                const datas = await res?.json();
+            } else {
+                setData(123);
+            }
         }
 
-    }, [data]);
+    }, [data, setData]);
+    console.log(data);
     return (
         <div>{`This is New ${data}`}</div>
     )

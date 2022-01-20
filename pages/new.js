2ee6b46033de22f0773a20e249452f9a637ab4ac
nick19890714 <83580980+nick19890714@users.noreply.github.com>
2022-01-20 14:29:34 +0800

    update

--- pages/new.js
+++ pages/new.js
@@ -5,16 +5,18 @@ const New = () => {
     const [data, setData] = useState();
     useEffect(async () => {
         if (!data) {
-            const res = await fetch('http://localhost:3000/api/hello').catch((error) => { console.log(error) });
+            const res = await fetch('http://localhost:3000/nextjs_github/api/hello').catch((error) => { console.log(error) });
             if (res?.status === 200) {
                 const datas = await res?.json();
+                console.log('datas', datas);
+                setData(datas.name);
             } else {
                 setData(123);
             }
         }
 
     }, [data, setData]);
-    console.log(data);
+    console.log('data', data);
     return (
         <div>{`This is New ${data}`}</div>
     )
